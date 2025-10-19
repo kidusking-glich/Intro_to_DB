fd482ceba6470205d1c6b1ec45c7f16f95f60558
kidusking-glich <abelyohannes69@gmail.com>
2025-10-19 21:00:59 +0300

    adding task_6.sql, the quirey do inserting multiple data into customer table

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+USE alx_book_store;
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUE (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+VALUE (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+VALUE (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
