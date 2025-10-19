e2dcd9affc64494a1b0e550b1fe8ee1d7e40efed
kidusking-glich <abelyohannes69@gmail.com>
2025-10-19 21:11:55 +0300

    removing spcace from id 2

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,9 @@
+-- SQL script to insert multiple rows into the 'customer' table of the 'alx_book_store' database.
+
+-- 1. Select the target database
 USE alx_book_store;
+
+-- 2. Insert the specified customer data using a single multi-row INSERT statement
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
